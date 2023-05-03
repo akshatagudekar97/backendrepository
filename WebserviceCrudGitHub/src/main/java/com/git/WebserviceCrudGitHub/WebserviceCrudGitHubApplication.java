4f7a84d69fa7287ca64e5dec02edc9f4bcd27041
akshatagudekar97 <akshatagudekar97@gmail.com>
2023-05-03 17:39:18 +0530

    web service done

--- WebserviceCrudGitHub/src/main/java/com/git/WebserviceCrudGitHub/WebserviceCrudGitHubApplication.java
+++ WebserviceCrudGitHub/src/main/java/com/git/WebserviceCrudGitHub/WebserviceCrudGitHubApplication.java
@@ -2,7 +2,10 @@ package com.git.WebserviceCrudGitHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+@EntityScan
+@EnableJpaRepositories
 @SpringBootApplication
 public class WebserviceCrudGitHubApplication {
 
